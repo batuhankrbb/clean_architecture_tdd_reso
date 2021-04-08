ad16dacb48a3f46e99c95228915f5b1ac672e6ea
batuhankrbb <batuhankarababa@gmail.com>
2021-04-08 15:10:28 +0300

    Video 3 Finished -Domain Layer Refactoring-

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,16 +1,37 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_clean_architecture_reso/core/error/failures.dart';
-import 'package:flutter_clean_architecture_reso/features/number_trivia/domain/entities/number_trivia.dart';
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecases.dart';
+import '../entities/number_trivia.dart';
 
 import '../repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+
+ //* Burada bi çıtır kafan karışmış olabilir knk normal çünkü param insanın gözünü korkutuyo. Neyse takma sen bunu. Kısacası Dönüş tipini belirttiğimiz içinde call metodu olan bi base use-case sınıfı yaptık onu implement ediyoruz artık. 
+ 
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
+  }
+}
+
+
+/*
+
   Future<Either<Failure, NumberTrivia>> call({@required int number}) async{
     return await repository.getConcreteNumberTrivia(number);
   }
-}
+  */
+
+/*
+ @override
+  Future<Either<Failure, NumberTrivia>> call(int number) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+*/
