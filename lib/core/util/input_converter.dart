433fe032c63bfa565405449085376fa2f82bf3f0
batuhankrbb <batuhankarababa@gmail.com>
2021-04-12 20:57:18 +0300

    Video 10 Finished -Bloc Scaffolding & Input Conversion

---
+++ lib/core/util/input_converter.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_clean_architecture_reso/core/error/failures.dart';
+
+class InputConverter {
+  Either<Failure, int> stringToUnsignedInteger(String str) {
+    try {
+      final parsedInteger = int.parse(str);
+      if (parsedInteger < 0) {
+        throw FormatException();
+      }
+
+      return Right(parsedInteger);
+    } on FormatException {
+      return Left(InvalidInputFailure());
+    }
+  }
+}
+
+class InvalidInputFailure extends Failure {}
